798423b3be637859f3ac8f7afd674c6f48c810bc
sirfitz <sirromariof@gmail.com>
2017-02-06 05:16:50 +0000

    lab submit

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for
-
+import time
 
 ###
 # Routing for your application.
@@ -25,6 +25,15 @@ def about():
     return render_template('about.html', name="Mary Jane")
 
 
+@app.route('/profile/')
+def profile():
+    return render_template('profile.html')
+
+def timeinfo():
+    now = time.strftime(" %a"+ "," +" %d %b %Y")
+    return render_template('profile.html', date= now)
+    
+    
 ###
 # The functions below should be applicable to all Flask apps.
 ###
